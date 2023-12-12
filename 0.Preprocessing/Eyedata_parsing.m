2b42c6e8c83688bc5cb3c895e540dbc8648db74f
Jaemin Seol <43691888+JAEMINSEOL@users.noreply.github.com>
2023-12-12 16:21:44 +0900

    Add files via upload

---
+++ 0.Preprocessing/Eyedata_parsing.m
@@ -0,0 +1,46 @@
+% eye_data_preprocessing
+ROOT.Mother = 'D:\NHP project';
+ROOT.Program = [ROOT.Mother '\Program'];
+addpath(genpath(ROOT.Program))
+
+
+%% file import
+
+date = '20200108';
+Animal_id = 'Nabi';
+Task_id = 'Cxt-Obj Association_6 Objects';
+File_id = 'Eye_Analog_Adj';
+
+ROOT.Datapixx = [ROOT.Mother '\Data\' Animal_id '\Behavior\' Task_id '\' date '\Datapixx'];
+ROOT.Unreal = [ROOT.Mother '\Data\' Animal_id '\Behavior\' Task_id '\' date '\Unreal'];
+
+fl = dir(ROOT.Datapixx); 
+for i=1:size(fl,1), if strncmp(fl(i).name,date,8), fl = fl(i).name; break; end; end
+
+ROOT.Datapixx = [ROOT.Datapixx '\' fl];
+
+[temp_list,Datapixx_eye] = get_dat_files(ROOT.Datapixx, fl, File_id);
+[temp_list_2,Datapixx_ticks] = get_dat_files(ROOT.Datapixx, fl, 'TickLog');
+[temp_list_3,Datapixx_events] = get_dat_files(ROOT.Datapixx, fl, 'EventLog');
+UE_raw = readtable([ROOT.Unreal '\' [Animal_id '_' date '.csv']]);
+UE_ticks = UE_raw(strcmp(UE_raw.Var1,'Tick'),:);
+
+rs = find(strcmp(UE_raw.Var1,'RecordingStart'));
+ re = find(strcmp(UE_raw.Var1,'RecordingEnd'));
+ UE_recording = [rs, re];
+
+
+UE_parsed = UE_raw(find(strcmp(UE_raw.Var1,'SessionStart')):find(strcmp(UE_raw.Var1,'SessionEnd')),:);
+
+ UE_ts = UE_raw(find(strcmp(UE_parsed.Var1,'TrialStart')),:);
+
+  Datapixx_events = Datapixx_events(find(strcmp(Datapixx_events,'SessionStart')),:);
+ Datapixx_ts = Datapixx_events(find(strcmp(Datapixx_events,'TrialStart')),:);
+
+  UE_recorded =[];
+ for i=1:size(UE_recording,1)
+ UE_recorded =  [UE_recorded; [UE_raw(UE_recording(i,1):UE_recording(i,2),:)]];
+     end
+
+
+%%
---
+++ modules/get_dat_files.m
@@ -0,0 +1,26 @@
+function [file_list,S0] = get_dat_files(root, date_id, file_id)
+temp = dir(root); file_list = {};
+for i=1:size(temp,1)
+    if contains(temp(i).name,date_id) & contains(temp(i).name,file_id)
+        x = find(temp(i).name=='_',1,'last');
+        n = str2double(temp(i).name(x+1:end-4));
+        file_list{n,1} = temp(i).name;
+    end
+end
+
+S0=[];
+for n=1:size(file_list,1)
+m1 = load(file_list{n}); fields = fieldnames(m1);
+
+if size(m1.([fields{1}]),1)>5
+    S0 = [S0; m1.([fields{1}])];
+else
+S0 = [S0; m1.([fields{1}])'];
+end
+
+end
+
+
+
+
+end
